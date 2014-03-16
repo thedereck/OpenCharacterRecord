395045aa1de2ac3d6bc36e82d7b28c0b65e62285
Dereck Curry <dereck.s.curry@gmail.com>
2014-03-16 12:34:20 -0400

    Initial Commit. Not Ready To Publish Yet.
    Just saving my work thus far. Not yet ready for publication and use by
    general public.

---
+++ OpenCharacterRecordUnitTest/OpenCharacterRecordXsdUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenCharacterRecordUnitTest
+{
+    [TestClass]
+    public class OpenCharacterRecordXsdUnitTests
+    {
+        [TestMethod]
+        public void TestMethod0001()
+        {
+        }
+    }
+}
