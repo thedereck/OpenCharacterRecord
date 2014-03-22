6ec39d4519fc6bc5503274c70f5f04ad1d4c43b7
Dereck Curry <dereck.s.curry@gmail.com>
2014-03-22 15:11:34 -0400

    More Human Fighter Unit Tests

--- UnitTests/UnitTests.cs
+++ UnitTests/UnitTests.cs
@@ -23,84 +23,119 @@ namespace UnitTests
         [TestMethod]
         public void HumanFighter0001()
         {
-            string valid = Validate(@"XmlTestFiles\BFRGP2-Human-Fighter-0001.xml"); 
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0001.xml"); 
             Assert.AreEqual("VALID", valid, false, valid);
         }
 
         [TestMethod]
         public void HumanFighter0002()
         {
-            string valid = Validate(@"XmlTestFiles\BFRGP2-Human-Fighter-0002.xml");
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0002.xml");
             Assert.AreNotEqual("VALID", valid, false, valid);
         }
 
         [TestMethod]
         public void HumanFighter0003()
         {
-            string valid = Validate(@"XmlTestFiles\BFRGP2-Human-Fighter-0003.xml");
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0003.xml");
             Assert.AreNotEqual("VALID", valid, false, valid);
         }
 
         [TestMethod]
         public void HumanFighter0004()
         {
-            string valid = Validate(@"XmlTestFiles\BFRGP2-Human-Fighter-0004.xml");
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0004.xml");
             Assert.AreNotEqual("VALID", valid, false, valid);
         }
 
         [TestMethod]
         public void HumanFighter0005()
         {
-            string valid = Validate(@"XmlTestFiles\BFRGP2-Human-Fighter-0005.xml");
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0005.xml");
             Assert.AreEqual("VALID", valid, false, valid);
         }
 
         [TestMethod]
         public void HumanFighter0006()
         {
-            string valid = Validate(@"XmlTestFiles\BFRGP2-Human-Fighter-0006.xml");
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0006.xml");
             Assert.AreNotEqual("VALID", valid, false, valid);
         }
 
         [TestMethod]
         public void HumanFighter0007()
         {
-            string valid = Validate(@"XmlTestFiles\BFRGP2-Human-Fighter-0007.xml");
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0007.xml");
             Assert.AreNotEqual("VALID", valid, false, valid);
         }
 
         [TestMethod]
         public void HumanFighter0008()
         {
-            string valid = Validate(@"XmlTestFiles\BFRGP2-Human-Fighter-0008.xml");
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0008.xml");
             Assert.AreNotEqual("VALID", valid, false, valid);
         }
 
         [TestMethod]
         public void HumanFighter0009()
         {
-            string valid = Validate(@"XmlTestFiles\BFRGP2-Human-Fighter-0009.xml");
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0009.xml");
+            Assert.AreNotEqual("VALID", valid, false, valid);
+        }
+
+        [TestMethod]
+        public void HumanFighter0010()
+        {
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0010.xml");
+            Assert.AreNotEqual("VALID", valid, false, valid);
+        }
+
+        [TestMethod]
+        public void HumanFighter0011()
+        {
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0011.xml");
+            Assert.AreNotEqual("VALID", valid, false, valid);
+        }
+
+        [TestMethod]
+        public void HumanFighter0012()
+        {
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0012.xml");
+            Assert.AreNotEqual("VALID", valid, false, valid);
+        }
+
+        [TestMethod]
+        public void HumanFighter0013()
+        {
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0013.xml");
+            Assert.AreNotEqual("VALID", valid, false, valid);
+        }
+
+        [TestMethod]
+        public void HumanFighter0014()
+        {
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Fighter-0014.xml");
             Assert.AreNotEqual("VALID", valid, false, valid);
         }
 
         [TestMethod]
         public void HumanCleric0001()
         {
-            string valid = Validate(@"XmlTestFiles\BFRGP2-Human-Cleric-0001.xml");
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Cleric-0001.xml");
             Assert.AreEqual("VALID", valid, false, valid);
         }
 
         [TestMethod]
         public void HumanMagicUser0001()
         {
-            string valid = Validate(@"XmlTestFiles\BFRGP2-Human-MagicUser-0001.xml");
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-MagicUser-0001.xml");
             Assert.AreEqual("VALID", valid, false, valid);
         }
 
         [TestMethod]
         public void HumanThief0001()
         {
-            string valid = Validate(@"XmlTestFiles\BFRGP2-Human-Thief-0001.xml");
+            string valid = Validate(@"XmlTestFiles\BFRPG2-Human-Thief-0001.xml");
             Assert.AreEqual("VALID", valid, false, valid);
         }
     }
